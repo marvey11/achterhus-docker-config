937fc2cf9ec65ae6ebee37abddff7b54fe6f9169
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-02-19 09:41:40 +0000

    fix: exclude Samba's .deleted folder from backup

--- scripts/backup-drives.sh
+++ scripts/backup-drives.sh
@@ -11,8 +11,12 @@ mkdir -p $DEST
 echo "--- Backup Started: $(date) ---" >> $LOG
 
 # rsync command: 
-# -a (archive), -v (verbose), -h (human readable), -z (compress during transfer)
+# -a (archive) 
+# -v (verbose) 
+# -h (human-readable) 
+# -z (compress during transfer)
 # --delete (remove files at destination that are gone from source)
-rsync -avhz --delete --exclude 'lost+found' $SOURCE $DEST >> $LOG 2>&1
+# --exclude (exclude specific directories, in this case 'lost+found' and '.deleted/')
+rsync -avhz --delete --exclude='lost+found' --exclude='.deleted/' $SOURCE $DEST >> $LOG 2>&1
 
 echo "--- Backup Finished: $(date) ---" >> $LOG
